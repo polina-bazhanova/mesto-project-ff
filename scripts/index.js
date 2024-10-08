e44bd0c78c22361d4ce82d4d31b4621b78db91f9
Polina Bazhanova <polina.bazhanov@yandex.kz>
2024-10-08 16:35:27 +0500

    Добавлен функционал по выводу и удалению карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const container = document.querySelector('.places');
+const cardContainer = container.querySelector('.places__list');
 
-// @todo: DOM узлы
+function renderCards(initialCard, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = initialCard.link;
+  cardElement.querySelector('.card__title').textContent = initialCard.name;  
+  const removeButton = cardElement.querySelector('.card__delete-button')
+  removeButton.addEventListener('click', function() {
+    deleteCard(cardElement);
+  });
+  cardContainer.append(cardElement);
+}
 
-// @todo: Функция создания карточки
+initialCards.forEach(cardData => {
+  renderCards(cardData, deleteCards);
+});
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+function deleteCards(cardElement) {
+  cardElement.remove();
+}
